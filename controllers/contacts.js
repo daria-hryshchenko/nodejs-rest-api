f4c2384242d8b48b74a8f11a694715035ba6b9eb
Daria Hryshchenko <daria199210@gmail.com>
2023-05-11 16:52:59 +0300

    connect to mongodb compass

---
+++ controllers/contacts.js
@@ -0,0 +1,18 @@
+const Contact = require("./../models/contact");
+
+const { HttpError, ctrlWrapper } = require("./../helpers/index");
+
+const listContacts = async (req, res) => {
+  const data = await Contact.find();
+  res.json(data);
+};
+
+const addContact = async (req, res) => {
+  const data = await Contact.create(req.body);
+  res.status(201).json(data);
+};
+
+module.exports = {
+  listContacts: ctrlWrapper(listContacts),
+  addContact: ctrlWrapper(addContact),
+};
---
+++ models/contact.js
@@ -0,0 +1,46 @@
+const { Schema, model } = require("mongoose");
+const Joi = require("joi");
+
+const { handleMongooseError } = require("../helpers");
+
+const contactSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+    },
+    favorite: {
+      type: Boolean,
+      required: false,
+    },
+  },
+  { versionKey: false, timestamps: true }
+);
+
+contactSchema.post("save", handleMongooseError);
+
+const addSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().required(),
+  phone: Joi.string().required(),
+  favorite: Joi.string(),
+});
+
+const schemas = {
+  addSchema,
+};
+
+const Contact = model("contact", contactSchema);
+
+module.exports = {
+  Contact,
+  schemas,
+};
